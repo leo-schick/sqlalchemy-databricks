e33e18c42a9a86fb4ba45f8329972c8ee2c8f163
Leo Schick <leonhard.schick@gmail.com>
2022-12-01 10:37:11 +0100

    support precision and scale for NUMERIC type

--- sqlalchemy_databricks/_type.py
+++ sqlalchemy_databricks/_type.py
@@ -2,6 +2,18 @@ from pyhive.sqlalchemy_hive import HiveTypeCompiler
 
 
 class DatabricksTypeCompiler(HiveTypeCompiler):
+
+    def visit_NUMERIC(self, type_):
+        precision = getattr(type_, "precision", None)
+        if precision is None:
+            return "DECIMAL"
+        else:
+            scale = getattr(type_, "scale", None)
+            if scale is None:
+                return "DECIMAL(%(precision)s)" % {"precision": precision}
+            else:
+                return "DECIMAL(%(precision)s, %(scale)s)" % {"precision": precision, "scale": scale}
+
     def visit_DATE(self, type_):
         # Woraround because pyhive uses TIMESTAMP instead of DATE.
         # See as well: https://github.com/dropbox/PyHive/issues/139
